09d473cc9f70e4a629c7bc7a4f31f84485c2b7fa
Mathias Bustamante <mbc@Mathiass-MacBook-Pro.local>
2013-10-23 12:06:03 -0500

    Now adding a wrapper class which adds more flexibility, still need to update README.md

--- ezalchemy.py
+++ ezalchemy.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
-import importlib
 
 from sqlalchemy import MetaData, create_engine, Table
 from sqlalchemy.orm import create_session, sessionmaker
@@ -11,60 +10,6 @@ __email__ = "mathiasbc@gmail.com"
 
 # dialect+driver://username:password@host:port/database
 SKELETON_URL = '%(dialect_driver)s://%(user)s:%(pass)s@%(host)s/%(database)s'
-
-# global variable session
-session = None
-
-
-class BaseTable(object):
-    '''Base table class for tables to be declared by the user'''
-
-    def create(self, **params):
-        '''Wraps the try: commit() except: rollback for easier usage'''
-        obj = self.__class__(**params)
-        session.add(obj)
-        try:
-            session.commit()
-            return obj
-        except:
-            session.rollback()
-            raise
-
-
-def connect(db_user, db_password, db_hostname, db_database, d_n_d='mysql'):
-    '''Builds the URL to connect to the database and connects'''
-    DB_URL = SKELETON_URL % {
-        'dialect_driver': d_n_d,
-        'user': db_user,
-        'pass': db_password,
-        'host': db_hostname,
-        'database': db_database, 
-    }
-
-    #Create and engine and get the metadata
-    Base = declarative_base()
-    engine = create_engine(DB_URL)
-    metadata = MetaData(bind=engine)
-    metadata.reflect(engine)
-
-    #Create a session to use the tables
-    Session = sessionmaker(bind=engine)
-    global session
-    session = Session()
-
-    # Get the name of the corrunt module
-    current_module = importlib.import_module(__name__)
-
-    # iterate over tables and append them to current module
-    for tablename in metadata.tables.keys():
-        # dynamically define new Table classes
-        new_class = type(str(tablename), (Base, BaseTable),
-            dict(
-                __table__=Table(tablename, metadata, autoload=True)
-            )
-        )
-        # add new Table class to module
-        setattr(current_module, new_class.__name__, new_class)
 
 
 class EZAlchemy(object):
@@ -103,9 +48,17 @@ class EZAlchemy(object):
             # add new Table class to module
             setattr(self, new_class.__name__, new_class)
 
-    def connect(self, tables):
+    def connect(self, tables_list):
         '''Reflects only the tables specified in tables list'''
-        raise NotImplemented
+        for tablename in tables_list:
+            # dynamically define new Table classes
+            new_class = type(str(tablename), (self.Base, object),
+                dict(
+                    __table__=Table(tablename, self.metadata, autoload=True)
+                )
+            )
+            # add new Table class to module
+            setattr(self, new_class.__name__, new_class)
 
     def insert(self, tablename, **params):
         '''inserts a new row to the table on database'''
